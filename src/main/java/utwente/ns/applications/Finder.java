cb5c43b59fb0bcb82dbcdc7af21b0432247c2b4f
Simon Struck <simonstruck70@gmail.com>
2017-04-10 19:14:23 +0200

    Added application structure

---
+++ src/main/java/utwente/ns/applications/Finder.java
@@ -0,0 +1,12 @@
+package utwente.ns.applications;
+
+/**
+ * Created by simon on 10.04.17.
+ */
+public class Finder implements IApplication {
+    
+    @Override
+    public void start() {
+    
+    }
+}
---
+++ src/main/java/utwente/ns/applications/IApplication.java
@@ -0,0 +1,8 @@
+package utwente.ns.applications;
+
+/**
+ * Created by simon on 10.04.17.
+ */
+public interface IApplication {
+    void start();
+}
