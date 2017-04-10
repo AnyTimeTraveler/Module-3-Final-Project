664851d2b1e4c1871ec8b0a963f5a8f5b65875d0
Simon Struck <simonstruck70@gmail.com>
2017-04-10 16:19:33 +0200

    Added different method of getting link cost

--- src/main/java/utwente/ns/linklayer/LinkLayer.java
+++ src/main/java/utwente/ns/linklayer/LinkLayer.java
@@ -16,14 +16,14 @@ import java.util.List;
  * Created by simon on 07.04.17.
  */
 public class LinkLayer implements Closeable {
-
+    
     private List<IReceiveListener> packetListeners;
     private InetAddress address;
     private MulticastSocket socket;
     private Thread receiver;
     private boolean closed;
     private int maxSegmentSize;
-
+    
     public LinkLayer(int maxSegmentSize) throws IOException {
         this.maxSegmentSize = maxSegmentSize;
         packetListeners = new ArrayList<>();
@@ -34,24 +34,24 @@ public class LinkLayer implements Closeable {
         receiver.setName("LinkLayerReceiver");
         receiver.start();
     }
-
+    
     public void send(IPacket packet) throws IOException {
         send(packet.marshal());
     }
-
+    
     public void send(byte[] data) throws IOException {
         DatagramPacket sendPacket = new DatagramPacket(data, data.length, address, Config.getInstance().getMulticastPort());
         socket.send(sendPacket);
     }
-
+    
     public void addReceiveListener(IReceiveListener receiver) {
         packetListeners.add(receiver);
     }
-
+    
     public void removeReceiveListener(IReceiveListener receiver) {
         packetListeners.remove(receiver);
     }
-
+    
     private void waitForIncomingPackets() {
         closed = false;
         while (!closed) {
@@ -67,12 +67,16 @@ public class LinkLayer implements Closeable {
             }
         }
     }
-
+    
     public int getLinkCost(String address) {
         return 1;
     }
     
-    public InetAddress getLocalAddress(){
+    public int getLinkCost(InetAddress address) {
+        return 1;
+    }
+    
+    public InetAddress getLocalAddress() {
         return socket.getLocalAddress();
     }
     
