b93fb85f5035d76249dd0a84a679dc1e242ea322
Niels_Overkamp <niels.overkamp@gmail.com>
2017-04-20 13:50:09 +0200

    This should fix the infinite resends

--- src/main/java/utwente/ns/tcp/RTP4Connection.java
+++ src/main/java/utwente/ns/tcp/RTP4Connection.java
@@ -440,6 +440,7 @@ public class RTP4Connection implements Closeable, IReceiveListener {
             while (state != RTP4Layer.ConnectionState.CLOSED && state != RTP4Layer.ConnectionState.TIME_WAIT) {
                 Thread.sleep(100);
                 if (System.currentTimeMillis() - timeStart > LISTEN_TIMEOUT_MILLIS) {
+                    forceClose();
                     throw new IOException("Timed out while closing");
                 }
             }
